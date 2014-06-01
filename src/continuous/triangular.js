3feb324ed6825c17cb8ee232a4500836e8eb966a
Eric Theise <erictheise@gmail.com>
2014-06-01 15:11:50 -0700

    triangular dist

--- src/continuous/triangular.js
+++ src/continuous/triangular.js
@@ -1,26 +1,38 @@
-// # Triangular Distribution
-//
-// http://en.wikipedia.org/wiki/Triangular_distribution
+/*
+ # Triangular Distribution
+
+ The [Triangular Distribution](http://en.wikipedia.org/wiki/Triangular_distribution) is a family of continuous probability distributions characterized by a single mode bracketed by minimum and maximum values.
+
+ ### Assumptions
+
+ `a`, `b`, and `c` are real numbers, the minimum, maximum, and modal values, with a < c < b; a may be thought of as a location parameter, (b - a) as a scale parameter, c as a shape parameter.
+
+ ### Use
+
+ `mctad.triangular(a, b, c)`
+*/
 
 mctad.triangular = function (a, b, c) {
   // Check that `a < c < b`.
   if (a >= b || a >= c || c >= b) { return undefined; }
 
-  var pmf, x, dfs = {
-    mean: (a + b + c)/3,
+  var dfs = {
+    mean: (a + b + c) / 3,
     median: (function () {
-      if (c > (a + b)/2) {
-        return a + Math.sqrt((b - a) * (c - a))/Math.sqrt(2);
+      if (c > (a + b) / 2) {
+        return a + Math.sqrt((b - a) * (c - a)) / Math.sqrt(2);
       } else {
-        return b - Math.sqrt((b - a) * (b - c))/Math.sqrt(2);
+        return b - Math.sqrt((b - a) * (b - c)) / Math.sqrt(2);
       }
     })(),
     mode: c,
-    variance: (Math.pow(a, 2) + Math.pow(b, 2) + Math.pow(c, 2) - (a * b) - (a * c) - (b * c))/18,
-    skewness: (Math.sqrt(2) * (a + b - 2 * c) * (2 * a - b - c) * (a - 2 * b + c))/(5 * Math.pow((Math.pow(a, 2) + Math.pow(b, 2) + Math.pow(c, 2) - a * b - a * c - b * c), 1.5)),
+    variance: (Math.pow(a, 2) + Math.pow(b, 2) + Math.pow(c, 2) - (a * b) - (a * c) - (b * c)) / 18,
+    skewness: (Math.sqrt(2) * (a + b - (2 * c)) * ((2 * a) - b - c) * (a - (2 * b) + c)) / (5 * Math.pow(Math.pow(a, 2) + Math.pow(b, 2) + Math.pow(c, 2) - (a * b) - (a * c) - (b * c), 1.5)),
+    entropy: 0.5 + Math.log((b - a) / 2),
     domain: { min: a, max: b },
+
     // `mctad.triangular(1, 4, 2).generate(100)` will generate an Array of 100
-    // random variables, distributed triangularly between 1 and 4, with peak 2.
+    // random variables, distributed triangularly between 1 and 4, with a peak/mode of 2.
     generate: function (n) {
       var randomVariables = [];
       for (var k = 0; k < n; k++ ) {
@@ -32,39 +44,39 @@ mctad.triangular = function (a, b, c) {
         }
       }
       return randomVariables;
-    }
-  };
+    },
 
-  pmf = function () {
-    if (a <= x && x <= c) {
-      return (2 * (x - a))/((b - a ) * (c - a));
-    } else {
-      if (c < x && x <= b) {
-        return (2 * (b - x))/((b - a ) * (b - c));
-      } else {
-        return 0;
-      }
-    }
-  };
-  cdf = function () {
-    if (x < a) {
-      return 0;
-    } else {
+    pdf: function (x) {
       if (a <= x && x <= c) {
-        return (Math.pow((x - a), 2))/((b - a ) * (c - a));
+        return (2 * (x - a)) / ((b - a ) * (c - a));
       } else {
         if (c < x && x <= b) {
-          return 1 - ((Math.pow((b - x), 2))/((b - a ) * (b - c)));
+          return (2 * (b - x)) / ((b - a ) * (b - c));
         } else {
-          return 1;
+          return 0;
+        }
+      }
+    },
+
+    cdf: function (x) {
+      if (x < a) {
+        return 0;
+      } else {
+        if (a <= x && x <= c) {
+          return (Math.pow((x - a), 2)) / ((b - a ) * (c - a));
+        } else {
+          if (c < x && x <= b) {
+            return 1 - ((Math.pow((b - x), 2)) / ((b - a ) * (b - c)));
+          } else {
+            return 1;
+          }
         }
       }
     }
   };
-  dfs[x] = {
-    pmf: pmf,
-    cdf: cdf
-  };
+
+  // Mix in the convenience methods for f(X) and F(X).
+  mctad.extend(dfs, mctad.continuousMixins);
 
   return dfs;
 };
--- test/continuous/triangular.test.js
+++ test/continuous/triangular.test.js
@@ -3,9 +3,72 @@ var assert = require('chai').assert;
 
 describe('triangular', function() {
 
-  it('can return undefined when a, b, and c are not valid parameters', function() {
-    assert.isUndefined(mctad.triangular(1, 2, 3), 'b should be >= c');
-    assert.isUndefined(mctad.triangular(2, 3, 1), 'c should be >= a');
-    assert.isUndefined(mctad.triangular(3, 1, 2), 'a should be <= b and c');
+  it('can return undefined when a, b, or c are not valid parameters', function() {
+    assert.isUndefined(mctad.triangular(1.0, 2.0, 3.0), 'b should be ≥ c');
+    assert.isUndefined(mctad.triangular(2.0, 3.0, 1.0), 'c should be ≥ a');
+    assert.isUndefined(mctad.triangular(3.0, 1.0, 2.0), 'a should be ≤ b and c');
   });
+
+  it('can generate the distribution statistics for a = 2.0, b = 6.0, c = 3.0', function() {
+    assert.isObject(mctad.triangular(2.0, 6.0, 3.0));
+    assert.closeTo(mctad.triangular(2.0, 6.0, 3.0).mean, 11/3, mctad.ε);
+    assert.closeTo(mctad.triangular(2.0, 6.0, 3.0).median, 3.55051, mctad.ε);
+    assert.closeTo(mctad.triangular(2.0, 6.0, 3.0).mode, 3.0, mctad.ε);
+    assert.closeTo(mctad.triangular(2.0, 6.0, 3.0).variance, 0.72222, mctad.ε);
+    assert.closeTo(mctad.triangular(2.0, 6.0, 3.0).skewness, 0.42240, mctad.ε);
+    assert.closeTo(mctad.triangular(2.0, 6.0, 3.0).entropy, 1.19315, mctad.ε);
+    assert.deepEqual(mctad.triangular(2.0, 6.0, 3.0).domain, { min: 2, max: 6 });
+  });
+
+  it('can generate the probability density function for a = 2.0, b = 6.0, c = 3.0', function() {
+    assert.equal(mctad.triangular(2.0, 6.0, 3.0).pdf(1.9), 0.0);
+    assert.equal(mctad.triangular(2.0, 6.0, 3.0).pdf(6.2), 0.0);
+    assert.equal(mctad.triangular(2.0, 6.0, 3.0).pdf(2.0), 0.0);
+    assert.closeTo(mctad.triangular(2.0, 6.0, 3.0).pdf(2.5), 0.25, mctad.ε);
+    assert.closeTo(mctad.triangular(2.0, 6.0, 3.0).pdf(3.0), 0.5, mctad.ε);
+    assert.closeTo(mctad.triangular(2.0, 6.0, 3.0).pdf(4.0), 0.33333, mctad.ε);
+    assert.closeTo(mctad.triangular(2.0, 6.0, 3.0).pdf(6.0), 0.0, mctad.ε);
+  });
+
+  it('can generate the cumulative probability distribution for a = 2.0, b = 6.0, c = 3.0', function() {
+    assert.equal(mctad.triangular(2.0, 6.0, 3.0).cdf(1.9), 0.0);
+    assert.equal(mctad.triangular(2.0, 6.0, 3.0).cdf(6.2), 1.0);
+    assert.closeTo(mctad.triangular(2.0, 6.0, 3.0).cdf(2.0), 0.0, mctad.ε);
+    assert.closeTo(mctad.triangular(2.0, 6.0, 3.0).cdf(3.0), 0.25, mctad.ε);
+    assert.closeTo(mctad.triangular(2.0, 6.0, 3.0).cdf(4.0), 0.66667, mctad.ε);
+    assert.closeTo(mctad.triangular(2.0, 6.0, 3.0).cdf(5.0), 11/12, mctad.ε);
+    assert.closeTo(mctad.triangular(2.0, 6.0, 3.0).cdf(6.0), 1.0, mctad.ε);
+  });
+
+  it('can generate the distribution statistics for a = -5.0, b = 5.0, c = 3.0', function() {
+    assert.isObject(mctad.triangular(-5.0, 5.0, 3.0));
+    assert.closeTo(mctad.triangular(-5.0, 5.0, 3.0).mean, 1.0, mctad.ε);
+    assert.closeTo(mctad.triangular(-5.0, 5.0, 3.0).median, 1.32456, mctad.ε);
+    assert.closeTo(mctad.triangular(-5.0, 5.0, 3.0).mode, 3.0, mctad.ε);
+    assert.closeTo(mctad.triangular(-5.0, 5.0, 3.0).variance, 4.66667, mctad.ε);
+    assert.closeTo(mctad.triangular(-5.0, 5.0, 3.0).skewness, -0.47614, mctad.ε);
+    assert.closeTo(mctad.triangular(-5.0, 5.0, 3.0).entropy, 2.10944, mctad.ε);
+    assert.deepEqual(mctad.triangular(-5.0, 5.0, 3.0).domain, { min: -5, max: 5 });
+  });
+
+  it('can generate the probability density function for a = -5.0, b = 5.0, c = 3.0', function() {
+    assert.equal(mctad.triangular(-5.0, 5.0, 3.0).pdf(-5.1), 0.0);
+    assert.equal(mctad.triangular(-5.0, 5.0, 3.0).pdf(5.2), 0.0);
+    assert.equal(mctad.triangular(-5.0, 5.0, 3.0).pdf(-5.0), 0.0);
+    assert.closeTo(mctad.triangular(-5.0, 5.0, 3.0).pdf(-3.0), 0.05, mctad.ε);
+    assert.closeTo(mctad.triangular(-5.0, 5.0, 3.0).pdf(0.6), 0.14, mctad.ε);
+    assert.closeTo(mctad.triangular(-5.0, 5.0, 3.0).pdf(4.0), 0.1, mctad.ε);
+    assert.closeTo(mctad.triangular(-5.0, 5.0, 3.0).pdf(6.0), 0.0, mctad.ε);
+  });
+
+  it('can generate the cumulative probability distribution for a = -5.0, b = 5.0, c = 3.0', function() {
+    assert.equal(mctad.triangular(-5.0, 5.0, 3.0).cdf(-5.1), 0.0);
+    assert.equal(mctad.triangular(-5.0, 5.0, 3.0).cdf(5.2), 1.0);
+    assert.equal(mctad.triangular(-5.0, 5.0, 3.0).cdf(-5.0), 0.0);
+    assert.closeTo(mctad.triangular(-5.0, 5.0, 3.0).cdf(-3.0), 0.05, mctad.ε);
+    assert.closeTo(mctad.triangular(-5.0, 5.0, 3.0).cdf(0.6), 0.392, mctad.ε);
+    assert.closeTo(mctad.triangular(-5.0, 5.0, 3.0).cdf(4.0), 0.95, mctad.ε);
+    assert.closeTo(mctad.triangular(-5.0, 5.0, 3.0).cdf(5.0), 1.0, mctad.ε);
+  });
+
 });
