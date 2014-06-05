9e5fd65a22cdb607ba80ef6988f1823072e75323
Eric Theise <erictheise@gmail.com>
2014-06-04 21:29:39 -0700

    erf refactor

--- src/continuous/erf.js
+++ src/continuous/erf.js
@@ -7,5 +7,7 @@ The implementation of the erf uses [Abramowitz and Stegun's approximation 7.1.28
 */
 
 mctad.erf = function (x) {
-  return 1 - (1 / Math.pow(1 + 0.0705230784 * x + 0.0422820123 * Math.pow(x, 2) + 0.0092705272 * Math.pow(x, 3) + 0.0001520143 * Math.pow(x, 4) + 0.0002765672 * Math.pow(x, 5) + 0.0000430638 * Math.pow(x, 6), 16));
+  return 1 - (1 / Math.pow( 1 + 0.0705230784 * x + 0.0422820123 * Math.pow(x, 2) +
+    0.0092705272 * Math.pow(x, 3) + 0.0001520143 * Math.pow(x, 4) + 0.0002765672 * Math.pow(x, 5) +
+    0.0000430638 * Math.pow(x, 6), 16));
 };
