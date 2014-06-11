15d3bea008301577397003cd620235776eef33fd
Eric Theise <erictheise@gmail.com>
2014-06-11 11:02:25 -0400

    textbook does not carry this out to enough decimal places, redo later

--- test/statistics/directional/mean_resultant_length.test.js
+++ test/statistics/directional/mean_resultant_length.test.js
@@ -14,6 +14,6 @@ describe('meanResultantLength', function() {
   it('can get the meanResultantLength of a biased roulette wheel (example 1.1)', function() {
       assert.closeTo(mctad.meanResultantLength([
         '43°', '45°', '52°', '61°', '75°', '88°', '88°', '279°', '357°'
-      ]), 0.711, mctad.ε);
+      ]), 0.711, 0.001);
   });
 });
